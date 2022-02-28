5c793d0d60905fce7795ef1d355d25787b971f17
T1xAn <pyps47.pyps47@yandex.ru>
2022-02-28 11:08:17 +0300

    Добавьте файлы проекта.

---
+++ lab2/lab2.cpp
@@ -0,0 +1,234 @@
+﻿// lab2.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "lab2.h"
+#include <windows.h>
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+static HWND baton;
+int screen_widh = 1920; int screen_height = 1080;
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+ HWND hWnd;
+ int key = 0, m = -1, xmove = 0, keymove = -20;
+
+ void paint_sprite(PAINTSTRUCT ps, HDC hdc, int key) {
+     if (xmove == 400 || xmove == 0) keymove = keymove*-1;
+         HBITMAP bmw = NULL;
+         HDC memdc;
+         xmove = xmove + keymove;
+     if (key == 0)
+         bmw = (HBITMAP)LoadImage(NULL, L"k0inv.bmp", IMAGE_BITMAP,
+             0, 0, LR_LOADFROMFILE);
+     
+     if (key ==1)
+         bmw = (HBITMAP)LoadImage(NULL, L"k1inv.bmp", IMAGE_BITMAP,
+             0, 0, LR_LOADFROMFILE);
+
+       
+         if (key == 2)
+             bmw = (HBITMAP)LoadImage(NULL, L"k2inv.bmp", IMAGE_BITMAP,
+                 0, 0, LR_LOADFROMFILE);
+
+       
+     memdc = CreateCompatibleDC(hdc);
+     SelectObject(memdc, bmw);
+     BitBlt(hdc, 500+xmove, 500, 188, 120, memdc, 0, 0, SRCINVERT);
+     Sleep(1000);
+     BitBlt(hdc, 500+ xmove, 500, 188, 120, memdc, 0, 0, SRCINVERT);
+     DeleteDC(memdc);
+     UpdateWindow(hWnd);
+ };
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LAB2, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB2));
+
+    MSG msg;
+   
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB2));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_LAB2);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+ 
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        SetTimer(hWnd, 3, 500, NULL);
+        return 0;
+      case WM_TIMER:
+          if (key == 2 || key == 0)
+              m = m * -1;
+          key = key + m;
+        InvalidateRect(hWnd, NULL, FALSE);
+        //UpdateWindow(hWnd);
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+      
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+        
+       
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            HBITMAP fon;
+          
+            fon = (HBITMAP)LoadImage(NULL, L"fon0.bmp", IMAGE_BITMAP,
+                0, 0, LR_LOADFROMFILE);
+           // DWORD err = ::GetLastError();
+            HDC fondc;
+            fondc = CreateCompatibleDC(hdc);
+            SelectObject(fondc, fon);
+       
+            BitBlt(hdc, 0, 0, screen_widh, screen_height, fondc, 0, 0, SRCCOPY);
+
+             paint_sprite(ps, hdc, key);
+    
+            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ lab2/lab2.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "resource.h"
+#include "resource.h"
+#include "fstream"
+#include "fstream"
+#include <vector>
+#include <string>
